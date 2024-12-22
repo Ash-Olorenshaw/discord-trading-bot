4ef57e7d8402bfaa6ae39f31269c8c7881a34a5d
Ash Olorenshaw <asholorenshaw@gmail.com>
2024-12-22 19:28:31 +1100

    Moved utility functions from main.py to utils.py

--- Buy and Sell bot/bot.py
+++ Buy and Sell bot/bot.py
@@ -4,6 +4,7 @@ import discord
 import matplotlib.pyplot as plt
 import datetime
 import time
+from utils import check_list_items_contain, convert_list_to_string, create_chopped_list, tenpercentchange
 
 from dotenv import load_dotenv
 
@@ -22,32 +23,6 @@ intentz.message_content = True
 intentz.members = True
 
 client = discord.Client(intents = intentz)
-
-def check_list_items_contain(list, item):
-    nameLen = len(item)
-    for line in list:
-        if line[:nameLen].lower() == item.lower():
-            return list.index(line)
-    return -1
-
-def create_chopped_list(list, item):
-    newList = []
-    itemLen = len(item)
-    for i in list:
-        newList.append(i[:itemLen])
-    return newList
-
-def convert_list_to_string(list):
-    s = ""
-    for i in list:
-        s += i
-        if i != list[-1]:
-            s += "-"
-    return s
-
-def tenpercentchange(ten, value):
-    newval = str(round(random.uniform(-1 * ten, ten) + float(value), 2))
-    return newval
 
 def price_refactor(prlist, dateseed, override):
     #random spikes need to be added
---
+++ Buy and Sell bot/utils.py
@@ -0,0 +1,29 @@
+import random
+
+def check_list_items_contain(list, item):
+    nameLen = len(item)
+    for line in list:
+        if line[:nameLen].lower() == item.lower():
+            return list.index(line)
+    return -1
+
+def create_chopped_list(list, item):
+    newList = []
+    itemLen = len(item)
+    for i in list:
+        newList.append(i[:itemLen])
+    return newList
+
+def convert_list_to_string(list):
+    s = ""
+    for i in list:
+        s += i
+        if i != list[-1]:
+            s += "-"
+    return s
+
+def tenpercentchange(ten, value):
+    newval = str(round(random.uniform(-1 * ten, ten) + float(value), 2))
+    return newval
+
+
