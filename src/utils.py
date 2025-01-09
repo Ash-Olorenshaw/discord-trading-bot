f9f898959c23ae8029863fee469f195b4b247652
Ash Olorenshaw <asholorenshaw@gmail.com>
2025-01-09 23:09:56 +1100

    Improved utils.py to make it much smarter/safer
    - Stopped using the variable name list because that's just stupid
    
    - Improved check_list_items_contain to now properly check for a full name rather than just the starting chars
    
    - A little preparation done to remove the convert_list_to_string func which is completely superfluous because of string.join()

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,25 @@
 import random
 
-def check_list_items_contain(list, item):
-    nameLen = len(item)
-    for line in list:
-        if line[:nameLen].lower() == item.lower():
-            return list.index(line)
+def check_list_items_contain(target_list, item):
+    #print("checking if {item} in {list}")
+    #nameLen = len(item)
+    for line in target_list:
+        if line.split("-")[0].lower() == item.lower():
+            return target_list.index(line)
     return -1
 
-def create_chopped_list(list, item):
+def create_chopped_list(target_list, item):
     newList = []
     itemLen = len(item)
-    for i in list:
+    for i in target_list:
         newList.append(i[:itemLen])
     return newList
 
-def convert_list_to_string(list):
+def convert_list_to_string(target_list):
     s = ""
-    for i in list:
+    for i in target_list:
         s += str(i)
-        if i != list[-1]:
+        if i != target_list[-1]:
             s += "-"
     return s
 
